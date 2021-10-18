30686977403e79ef405e61625e2f594436039f8b
HeDoBeCodinginPythonTho <66318498+HeDoBeCodinginPythonTho@users.noreply.github.com>
2021-10-17 20:59:25 -0400

    Add files via upload

---
+++ exercise5.py
@@ -0,0 +1,10 @@
+xList = [10, 20, 30, 40, 10]
+yList = [75, 65, 35, 75, 30]
+if xList[0] == xList[0-1]:
+    print("True")
+elif xList[0] != xList[0-1]:
+    print("False")
+if yList[0] == yList[0-1]:
+    print("True")
+elif yList[0] != yList[0-1]:
+    print("False")
